a5e099d873eb88ead6750a0ea4570d773335784c
dardonosti <dardonostiok@gmail.com>
2019-07-24 22:24:16 -0300

    Componente principal

---
+++ frontend/src/app/principal/principal.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit {
+  config: any;
+  fullpage_api: any;
+
+  constructor() {
+
+    // for more details on config options please visit fullPage.js docs
+    this.config = {
+
+      // fullpage options
+      licenseKey: null,
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+      menu: '#menu',
+
+      // fullpage callbacks
+      afterResize: () => {
+        console.log("After resize");
+      },
+      afterLoad: (origin, destination, direction) => {
+        console.log(origin.index);
+      }
+    };
+  }
+
+  getRef(fullPageRef) {
+    this.fullpage_api = fullPageRef;
+  }
+
+  ngOnInit() {
+  }
+
+}

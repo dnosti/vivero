6522320df20d1309291e37973820ce3aadf6e164
dardonosti <dardonostiok@gmail.com>
2019-07-31 19:47:53 -0300

    Actualización

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
-<<<<<<< HEAD
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CoreModule} from "./core/core.module";
@@ -13,25 +12,14 @@ import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminComponent } from './admin/admin.component';
 import { MainComponent } from './main/main.component';
-=======
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {CoreModule} from "./core/core.module";
-import { GraphComponent } from './graph/graph.component';
-import { ChartsModule } from 'ng2-charts';
->>>>>>> 3825264a6e166cb194e3cf35720cf94358e1f456
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginComponent,
     HomeComponent,
     AdminComponent,
     MainComponent
-=======
-    AdminComponent,
-    GraphComponent
->>>>>>> 3825264a6e166cb194e3cf35720cf94358e1f456
   ],
   imports: [
     BrowserModule,
@@ -40,14 +28,7 @@ import { ChartsModule } from 'ng2-charts';
     BrowserAnimationsModule,
     CoreModule,
     ReactiveFormsModule,
-<<<<<<< HEAD
     HttpClientModule,
-=======
-    ChartsModule
-  ],
-  exports: [
-    AdminComponent
->>>>>>> 3825264a6e166cb194e3cf35720cf94358e1f456
   ],
   providers: [],
   bootstrap: [AppComponent]
